2c21f935058d8bde65f18ca6534c5ecb76ca6d2a
Gustavo Harff <gustavo.harff@gmail.com>
2022-05-04 20:20:08 -0300

    chore: Ajustar estrutura do projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
-import React, { useState } from 'react';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-
   const [linhas, setLinhas] = useState(0);
   const [colunas, setColunas] = useState(0);
   const [matriz, setMatriz] = useState<any[][]>([[]]);
   const [gerar, setGerar] = useState(false);
   const array = [
-    { item: 'A' },
-    { item: 'Lo' },
-    { item: 'Le' },
-    { item: 'Ls' },
-    { item: 'E' },
-    { item: 'O' },
-    { item: 'S' },
-    { item: 'Lo' },
-    { item: 'Le' },
-    { item: 'Ls' },
-  ]
+    { item: "A" },
+    { item: "Lo" },
+    { item: "Le" },
+    { item: "Ls" },
+    { item: "E" },
+    { item: "O" },
+    { item: "S" },
+    { item: "Lo" },
+    { item: "Le" },
+    { item: "Ls" },
+  ];
 
   function ambiente() {
     let min = Math.ceil(1);
@@ -26,88 +25,112 @@ function App() {
     let num = Math.floor(Math.random() * (max - min)) + min;
     switch (num) {
       case 1:
-        return 'Lo';
+        return "Lo";
       case 2:
-        return 'Ls';
+        return "Ls";
       case 3:
-        return 'Le';
+        return "Le";
       case 4:
-        return ' ';
+        return " ";
       case 5:
-        return 's';
+        return "s";
       case 6:
-        return 'o';
+        return "o";
       case 6:
-        return 'e';
+        return "e";
     }
   }
 
   function gerarMatriz() {
-    let mt = Array(linhas).fill('').map(row => new Array(colunas).fill(''))
+    let mt = Array(linhas)
+      .fill("")
+      .map((row) => new Array(colunas).fill(""));
     let l = 0;
     let c = 0;
     for (l = 0; l < linhas; l++) {
       for (c = 0; c < colunas; c++) {
         if (l === 0 && c === 0) {
-          mt[l][c] = 'A';
+          mt[l][c] = "A";
         } else {
           mt[l][c] = ambiente();
         }
       }
     }
     console.table(mt);
-    setMatriz(mt)
-    setGerar(true)
-    let s : string
-    s = 2 + '2';
-    console.log(s)
+    setMatriz(mt);
+    setGerar(true);
+    let s: string;
+    s = 2 + "2";
+    console.log(s);
   }
-
-
 
   return (
     <div className="App">
       <header className="App-header">
-
-        {!gerar &&
+        {!gerar && (
           <>
             <label>Digite a quantidade de linhas</label>
-            <input type="number" defaultValue={0} value={linhas} onChange={(event) => { setGerar(false); setLinhas(parseInt(event.target.value)) }} id="linhas"></input>
+            <input
+              type="number"
+              defaultValue={0}
+              value={linhas}
+              onChange={(event) => {
+                setGerar(false);
+                setLinhas(parseInt(event.target.value));
+              }}
+              id="linhas"
+            ></input>
             <label>Digite a quantidade de colunas</label>
-            <input type="number" defaultValue={0} value={colunas} onChange={(event) => { setGerar(false); setColunas(parseInt(event.target.value)) }} id="colunas"></input>
-            <button style={{ color: 'blue', backgroundColor: 'white' }} onClick={() => { gerarMatriz() }}>Gerar</button>
+            <input
+              type="number"
+              defaultValue={0}
+              value={colunas}
+              onChange={(event) => {
+                setGerar(false);
+                setColunas(parseInt(event.target.value));
+              }}
+              id="colunas"
+            ></input>
+            <button
+              style={{ color: "blue", backgroundColor: "white" }}
+              onClick={() => {
+                gerarMatriz();
+              }}
+            >
+              Gerar
+            </button>
           </>
-        }
+        )}
         {gerar &&
           matriz.map((l, index1) => {
             return (
-              <div style={{ display: 'flex', flex: colunas, maxHeight: '42px', }}>
+              <div
+                style={{ display: "flex", flex: colunas, maxHeight: "42px" }}
+              >
                 {l.map((c, index2) => {
                   return (
                     <div
-                      id={'div_' + index1 + '_' + index2}
+                      id={"div_" + index1 + "_" + index2}
                       style={{
-                        color: 'blue',
-                        fontSize: '20px',
-                        backgroundColor: 'white',
-                        border: '1px solid black',
-                        width: '38px',
-                        height: '40px',
-                        alignItems: 'center',
-                        justifyContent: 'center'
+                        color: "blue",
+                        fontSize: "20px",
+                        backgroundColor: "white",
+                        border: "1px solid black",
+                        width: "38px",
+                        height: "40px",
+                        alignItems: "center",
+                        justifyContent: "center",
                       }}
                     >
                       {c}
                     </div>
-                  )
+                  );
                 })}
               </div>
-            )
-          })
-        }
-
-      </header >
-    </div >
+            );
+          })}
+      </header>
+    </div>
   );
 }
 
